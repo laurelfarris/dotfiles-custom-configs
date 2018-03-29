9a7b6516390ab0196725311f1409084909639ded
laurelfarris <laurel@laurelfarris.com>
2018-03-29 12:45:43 -0600

    zshrc

--- zshrc.sh
+++ zshrc.sh
@@ -1,5 +1,6 @@
 SAVEHIST=10000
 
+# Someone else's (Dustin's?) aliases.
 alias rOAun='./manage.py runserver 0.0.0.0:8000'
 alias mkenv='mkvirtualenv --python=/usr/local/bin/python3 `basename $PWD` && setvirtualenvproject'
 alias pycclean='find . -name "*.pyc" -exec rm {} \;'
@@ -8,8 +9,11 @@ alias sm='./manage.py schemamigration'
 alias mm='./manage.py makemigrations'
 #alias ipy='ipython --matplotlib --pylab'
 alias activate='source env/bin/activate'
-alias gs='git status'
 
+# Don't really need this alias (26 March 2018).
+#alias gs='git status'
+
+# My aliases
 alias src='source ~/dotfiles/custom-configs/zshrc.sh'
 alias chrome='open -n -a Google\ Chrome'
 alias ipy='ipython'
@@ -20,15 +24,18 @@ alias ll='ls -dFHGP ^*.(aux|bbl|blg|cb|cb2|lof|log|lot|maf|mtc*|nav|out|snm|toc)
 alias l1='ls -1dFHGP ^*.(aux|bbl|blg|cb|cb2|lof|log|lot|maf|mtc*|nav|out|snm|toc)'
 alias mv='mv -i'
 
-function ropen() {
-    for fname in $@; do
-        open ${HOME}/Google\ Drive/References/Mendeley/$fname*.pdf
-    done
+function nospaces() {
+    find . -mindepth 1 -maxdepth 1 -iname "*\ *" | while read line; do
+    old=${line:s/\*//}
+    new=` echo $old | tr ' ' '_' `
+    mv $old $new
+done
 }
 
 function safe_rm() {
     for fname in $@; do
         if [[ `basename $PWD` != .Trash ]]; then
+            touch -ac $fname
             command mv $fname ${HOME}/.Trash/
         else
             command rm $fname
@@ -37,21 +44,15 @@ function safe_rm() {
 }
 alias rm='safe_rm'
 
-function nospaces() {
-    ls -1 *\ * | while read line; do
-    old=${line:s/\*//}
-    new=` echo $old | tr ' ' '_' `
-    mv $old $new
-done
-}
-
-
-function cleanup() {
+# "Permanent rm"
+function prm() {
     # Permanently delete files moved to Trash more than 1 month ago.
+    nospaces
     find ${HOME}/.Trash -type f -atime +30 -delete
     find ${HOME}/.Trash -mindepth 1 -type d -empty -delete
 }
 
+# WTF is this? Might be something I copied off the interwebs (26 March 2018).
 function rename() {
     for f in treteupp.*; do
         filename="${f%.*}"
@@ -60,7 +61,7 @@ function rename() {
     done
 }
 
-# Open a pdf in preview without having to worry about deleting it later
+# Download pdf, open in preview, and immediately delete.
 function get() {
     dir=${HOME}/Temp/
     filenumber=`date +'%s'`
@@ -75,13 +76,13 @@ function get() {
 }
 
 
+# ???
 autoload -U zmv
 alias mmv='noglob zmv -W'
-
 export DJANGO_ENV=local
-
 export PATH="./node_modules/.bin:$PATH"
 
+# Can't remember if this was actually working or not... (26 March 2018).
 LS_COLORS="di=00;38;5;67:ln=00;38;5;167:ex=00;38;5;071:*.png=00;38;5;147:*.jpg=00;38;5;147:*.gz=00;38;5;215:*.tar=00;38;5;215"
 export LS_COLORS
 export LSCOLORS=exfxcxdxgxegedabagacad
